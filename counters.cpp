c1fe2276f7a1a356a5e62f85aa080b5460dfe23e
Chris Porter <slug@quakenet.org>
2008-09-13 05:50:45 +0100

    More comments.

--- counters.cpp
+++ counters.cpp
@@ -7,6 +7,10 @@
 
 #include "counters.h"
 
+/**********************************************
+ * PacketCounter
+ **********************************************/
+
 PacketCounter::PacketCounter(): counts() {
   /* EMPTY */
 }
